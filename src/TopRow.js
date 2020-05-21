10afb6ba6f69962283cf49775650a5574a8fee40
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2020-05-20 23:32:52 -0400

    Timer Working

--- src/TopRow.js
+++ src/TopRow.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import "./App.css";
 
 const TopRow = (props) => {
+    // variables Timer
+    const [minTimer, setMinTimer] = useState(4)
+    const [secTimer, setSecTimer] = useState('00')
+    const [secondsCount, SetSecondsCount] = useState(0)
+    const totalTimerSeconds = 240
+    useEffect(() => {
+        const timer = setInterval(() => {
+            if(secondsCount !== 0 && secondsCount <= 240) {
+                if(secTimer == '00') {
+                    setMinTimer(minTimer - 1)
+                    setSecTimer(59)
+                } else {
+                    if(secTimer > 10) {
+                        setSecTimer(secTimer-1)
+                    } else {
+                        setSecTimer(`0${secTimer-1}`)
+                    }
+                    
+                }
+            }
+            if(secondsCount <= totalTimerSeconds) {
+                SetSecondsCount(secondsCount + 1)
+                console.log(totalTimerSeconds,secondsCount);
+            }
+        }, 1000)
+        return () => clearInterval(timer)
+    },)
   return (
         <div className="topRow">
           <div className="home">
@@ -10,7 +37,7 @@ const TopRow = (props) => {
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
             <div className="home__score">{props.homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{`${minTimer}:${secTimer}`}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{props.visitorScore}</div>
